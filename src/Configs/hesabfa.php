f719f032f1c09e8e5f9d2928e25cc135e127f902
amirbagh75 <amirbagh75@gmail.com>
2020-12-23 12:53:45 +0330

    --

--- src/Configs/hesabfa.php
+++ src/Configs/hesabfa.php
@@ -1,12 +1,8 @@
 <?php
 
 return [
-    // SMS.ir Api Key
-    'id' => env('HESABFA_USER_ID', 'Your api key'),
-    // SMS.ir Secret Key
-    'password' => env('HESABFA_USER_PASSWORD', 'Your Username'),
-    // Your sms.ir line number
-    'key' => env('HESABFA_API_KEY', 'Your Password'),
-    //HTTP Request Timeout (seconds)
+    'id' => env('HESABFA_USER_ID', ''),
+    'password' => env('HESABFA_USER_PASSWORD', ''),
+    'key' => env('HESABFA_API_KEY', ''),
     'timeout'=> 10
 ];
